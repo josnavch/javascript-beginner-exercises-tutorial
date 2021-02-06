35397ec0d11fc91ab99984c494037ba0064253c8
josnavch <josnavch@gmail.com>
2021-02-06 03:43:04 +0000

    Exercises JavaScript  JN

--- exercises/03-Print-variables-in-the-console/app.js
+++ exercises/03-Print-variables-in-the-console/app.js
@@ -1,4 +1,8 @@
 var mySuperVariable = 'hello';
+var color = 'red';
 console.log(mySuperVariable);
 
 // Your code below:
+
+var color = 'red';
+console.log(color);
--- exercises/05-User-inputed-variables/app.js
+++ exercises/05-User-inputed-variables/app.js
@@ -1,3 +1,9 @@
+
 var age = prompt('What is your age?');
 
 // Your code below:
+
+var age = parseInt(age, 10);
+
+var edad = age + 10;
+console.log("Su edad es: " + age);
