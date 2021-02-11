9deba2237146483e0311a7da3a2dbbbed3ec706c
josnavch <josnavch@gmail.com>
2021-02-11 03:05:21 +0000

    exercises -13 JN

--- exercises/12-Your-first-if/app.js
+++ exercises/12-Your-first-if/app.js
@@ -1,3 +1,22 @@
 var total = prompt('How many km are left to go?');
 
 // Your code below:
+
+var km = parseInt(total, 10);
+var msg =  "";
+
+if (km > 100) {
+     msg =  "Are we there yet?"
+} else {
+    if (km > 50) {
+         msg =  "We'll be there in 5 minutes"
+    } else {
+        if (km <= 50){
+             msg =  "I'm parking, I see you right now"
+        }
+        
+    }
+    
+}
+
+console.log(msg)
