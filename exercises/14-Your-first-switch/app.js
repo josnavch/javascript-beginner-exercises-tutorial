ce6bdcaf4adbe744000db8d5107735daab1e6669
josnavch <josnavch@gmail.com>
2021-02-12 22:17:07 +0000

    Ex-14 JN

--- exercises/14-Your-first-switch/app.js
+++ exercises/14-Your-first-switch/app.js
@@ -1,15 +1,28 @@
 function getColor(selection)
 {
-	switch(selection){
-		// Add more options here
+    let zapatos = selection.toUpperCase();
+    console.log(zapatos);
+	switch(zapatos){
+        // Add more options here
+            case "RED":
+                return true;
+                break;
+            case "GREEN":
+                return true;
+                break;
+            case "BLUE":
+                console.log("Es azul el zapato");
+                return true;
+                break;
 	    default:
 	    	return false;  //returns false because the user picked an unavailable color
-	    break;               
 	}
 }
 
 var colorname = prompt('What color do you want?');
 var isAvailable = getColor(colorname);
+
+console.log(isAvailable);
 
 if(isAvailable)
 	console.log('Good news! That color is available');
